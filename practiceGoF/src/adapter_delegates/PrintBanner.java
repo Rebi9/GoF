6826c0c29552f92c12e3231106ba1e2f40059639
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-11-23 11:31:34 +0900

    add adapter_delegates pattern

---
+++ practiceGoF/src/adapter_delegates/PrintBanner.java
@@ -0,0 +1,17 @@
+package adapter_delegates;
+
+public class PrintBanner extends Print{
+	private Banner banner;
+
+	public PrintBanner(String string) {
+		this.banner = new Banner(string);
+	}
+
+	public void printWeak() {
+		banner.showWithParen();
+	}
+
+	public void printStrong() {
+		banner.showWithAster();
+	}
+}
