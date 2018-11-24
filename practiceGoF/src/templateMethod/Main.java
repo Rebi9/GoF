7b4a47f71358b347839c3d74ee4870c30c4e82f0
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-11-24 17:11:38 +0900

    add template method pattern

---
+++ practiceGoF/src/templateMethod/Main.java
@@ -0,0 +1,14 @@
+package templateMethod;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AbstractDisplay d1 = new CharDisplay('Q');
+		AbstractDisplay d2 = new StringDisplay("test");
+		AbstractDisplay d3 = new StringDisplay("demo");
+		d1.display();
+		d2.display();
+		d3.display();
+	}
+
+}
