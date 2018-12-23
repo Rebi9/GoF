81e3e8c4d3a96ededc170cf52236ced3913f9d12
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-12-23 15:51:20 +0900

    add builder pattern

---
+++ practiceGoF/src/builder/Builder.java
@@ -0,0 +1,8 @@
+package builder;
+
+public abstract class Builder {
+	public abstract void makeTitle(String string);
+	public abstract void makeString(String str);
+	public abstract void makeItems(String[] items);
+	public abstract void close();
+}
